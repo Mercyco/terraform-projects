96e3ce7c50d1b0a0c56f62d66aefc9ca7bcd1bd1
Mercy Onanike <mercyonanike@gmail.com>
2024-06-05 15:25:55 +0100

    created the security group file

--- variable.tf
+++ variable.tf
@@ -40,3 +40,10 @@ variable "vpc_cidr" {
     description  = "private data subnet az2 cidr block"
     type         = string
  }
+
+# security group variables
+variable "ssh_location"{
+    default      = "0.0.0.0/0"
+    description  = "the ip address that can ssh into the ecs instances"
+    type         = string
+ }
