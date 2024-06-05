bb95bc2d65fd2ad732ed2d5799f3216ba3f602aa
Mercy Onanike <mercyonanike@gmail.com>
2024-06-05 21:54:49 +0100

    created route-53 file

--- variable.tf
+++ variable.tf
@@ -112,3 +112,16 @@ variable "ec2_key_pair_name" {
     description  = "name of the ec2 key pair"
     type         = string
  }
+
+# route 53 variables
+variable "domain_name" {
+    default      = "dynamicshop.click"
+    description  = "domain name"
+    type         = string
+}
+
+variable "record_name" {
+    default      = "WWW"
+    description  = "sub domain name"
+    type         = string
+}
